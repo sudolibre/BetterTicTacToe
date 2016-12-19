f7ad98776841772ada615828dfdce4f2c87e5c82
sudolibre <optout.breaker@gmail.com>
2016-12-18 20:50:52 -0500

    initial commit

---
+++ BetterTicTacToe/Main.swift
@@ -0,0 +1,9 @@
+//
+//  Main.swift
+//  BetterTicTacToe
+//
+//  Created by Jonathon Day on 12/15/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import Foundation
---
+++ BetterTicTacToe/Match.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  BetterTicTacToe
+//
+//  Created by Jonathon Day on 12/16/16.
+//  Copyright © 2016 dayj. All rights reserved.
+//
+
+import Foundation
+
+struct Match {
+    let playerXName: String
+    let playerOName: String
+    
+    struct Player {
+        
+    }
+}
